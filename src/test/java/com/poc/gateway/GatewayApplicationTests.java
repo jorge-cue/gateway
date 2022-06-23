1406cb91dc269c77ee97832f0a95f76c1c8fbe2a
Jorge Cue <jorge.horacio.cantu@ibm.com>
2022-06-23 16:33:24 -0500

    Define smokeTest

--- src/test/java/com/poc/gateway/GatewayApplicationTests.java
+++ src/test/java/com/poc/gateway/GatewayApplicationTests.java
@@ -1,13 +1,23 @@
 package com.poc.gateway;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GatewayApplicationTests {
 
+    @Autowired
+    private WebTestClient webClient;
+
     @Test
-    void contextLoads() {
+    void smokeTest() {
+        webClient.get()
+                .uri("/actuator/health")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody().jsonPath("$.status").isEqualTo("UP");
     }
 
 }
