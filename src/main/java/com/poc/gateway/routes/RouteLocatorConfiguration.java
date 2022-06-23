9cfefc1ffdb1e5e67e2b8854a46c6667047500f8
Jorge Cue <jorge.horacio.cantu@ibm.com>
2022-06-23 16:39:13 -0500

    Route locator GET /get -> http://httpbin.org:80/get

---
+++ src/main/java/com/poc/gateway/routes/RouteLocatorConfiguration.java
@@ -0,0 +1,23 @@
+package com.poc.gateway.routes;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteLocatorConfiguration {
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("get", p -> p
+                        .method("GET")
+                        .and()
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://httpbin.org:80")
+                )
+                .build();
+    }
+}
