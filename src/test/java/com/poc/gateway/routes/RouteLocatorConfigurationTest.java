d1d662d47de9728e9e08fd2fedfe0a04d4ce1ba9
Jorge Cue <jorge.horacio.cantu@ibm.com>
2022-06-24 17:14:39 -0500

    Algunas pequeñas mejoras en Tests

--- src/test/java/com/poc/gateway/routes/RouteLocatorConfigurationTest.java
+++ src/test/java/com/poc/gateway/routes/RouteLocatorConfigurationTest.java
@@ -1,6 +1,7 @@
 package com.poc.gateway.routes;
 
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import com.github.tomakehurst.wiremock.matching.EqualToPattern;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,8 +12,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-        properties = "downstreamURI=http://localhost:${wiremock.server.port}"
-)
+        properties = "downstreamURI=http://localhost:${wiremock.server.port}")
 @WireMockTest
 class RouteLocatorConfigurationTest {
 
@@ -20,14 +20,17 @@ class RouteLocatorConfigurationTest {
     private WebTestClient webClient;
 
     @Test
-    void getRouteInjectsHelloHeader() {
-        stubFor(get("/get").willReturn(aResponse()
-                .withStatus(HttpStatus.OK.value())
-                .withBody("{\"headers\":[\"Hello\":\"World\"]}"))
+    void getRouteInjectsHelloWorldHeader() {
+        stubFor(get("/get").withHeader("Hello", new EqualToPattern("World")) // Header "Hello: World" is inserted by gateway
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("{\"headers\":[\"Hello\":\"World\"]}"))
         );
 
-        webClient.get().uri("/get")
+        webClient.get().uri("/get") // The Header "Hello: World" was added by the gateway
                 .exchange()
+                .expectHeader().contentType("application/json")
                 .expectStatus().isOk()
                 .expectBody().jsonPath("$.headers.Hello").isEqualTo("World");
     }
